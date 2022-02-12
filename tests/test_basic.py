25c89e5b49c94ce5fb701b8038014b1f2224db7b
Antony Polukhin <antoshkka@gmail.com>
2022-02-12 19:35:57 +0300

    simplify python

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,4 @@
-# Start via:
-# ~/.local/bin/pytest --build-dir=/data/code/service_template/build_debug
-
-
+# Start via `make test-debug` or `make test-release`
 async def test_basic(service_template_client):
     response = await service_template_client.post('/hello', json={})
     assert response.status == 200
