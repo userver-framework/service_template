e8950abc17415fc51e82952a2d7c6f97ab894d2d
Victor Makarov <vitja@yandex-team.ru>
2022-04-19 15:28:42 +0300

    Update testsuite integration (#8)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,4 @@
 # Start via `make test-debug` or `make test-release`
-async def test_basic(service_template_client):
-    response = await service_template_client.post('/hello', json={})
+async def test_basic(service_client):
+    response = await service_client.post('/hello', json={})
     assert response.status == 200
