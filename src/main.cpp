ab44dfc374ccc89ef932410dc93e71b946ad1cd4
Antony Polukhin <antoshkka@gmail.com>
2023-03-06 10:17:53 +0300

    order the headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <userver/components/minimal_server_component_list.hpp>
-#include <userver/server/handlers/ping.hpp>
-#include <userver/utils/daemon_run.hpp>
 #include <userver/clients/dns/component.hpp>
 #include <userver/clients/http/component.hpp>
-#include <userver/testsuite/testsuite_support.hpp>
+#include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
-
+#include <userver/testsuite/testsuite_support.hpp>
+#include <userver/utils/daemon_run.hpp>
 
 #include "hello.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
