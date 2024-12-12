c872c8a3c030d1251654b2dbbc2d8f884dc28dbb
Anton Zhilin <antonyzhilin@gmail.com>
2024-12-12 23:43:42 +0300

    Fix

--- cmake/DownloadUserver.cmake
+++ cmake/DownloadUserver.cmake
@@ -3,19 +3,12 @@ include_guard(GLOBAL)
 function(download_userver)
   set(OPTIONS)
   set(ONE_VALUE_ARGS TRY_DIR)
-  set(MULTI_VALUE_ARGS OPTIONS)
+  set(MULTI_VALUE_ARGS)
   cmake_parse_arguments(
       ARG "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}" ${ARGN}
   )
 
   if(ARG_TRY_DIR AND EXISTS "${ARG_TRY_DIR}")
-    foreach(OPTION IN LIST ARG_OPTIONS)
-      separate_arguments(OPTION)
-      list(GET OPTION 0 KEY)
-      list(GET OPTION 1 VALUE)
-      set("${KEY}" "${VALUE}" CACHE STRING "" FORCE)
-    endforeach()
-
     message(STATUS "Using userver from ${ARG_TRY_DIR}")
     add_subdirectory("${ARG_TRY_DIR}")
     return()
@@ -25,6 +18,5 @@ function(download_userver)
   CPMAddPackage(
       NAME userver
       ${ARG_UNPARSED_ARGUMENTS}
-      OPTIONS ${OPTIONS}
   )
 endfunction()
